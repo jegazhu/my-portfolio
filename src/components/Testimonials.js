405deb85aa8c145e8d76d731ec39cd68ae8f9e13
Jaime Gaspar <jegazhu@gmail.com>
2021-07-25 16:57:11 +0000

    updating testimonial for testimonials variable

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -11,23 +11,23 @@ export default function Testimonials() {
           Client Testimonials
         </h1>
         <div className="flex flex-wrap m-4">
-          {testimonials.map((testimonial) => (
+          {testimonials.map((testimonials) => (
             <div className="p-4 md:w-1/2 w-full">
               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
                 <TerminalIcon className="block w-8 text-gray-500 mb-4" />
-                <p className="leading-relaxed mb-6">{testimonial.quote}</p>
+                <p className="leading-relaxed mb-6">{testimonials.quote}</p>
                 <div className="inline-flex items-center">
                   <img
                     alt="testimonial"
-                    src={testimonial.image}
+                    src={testimonials.image}
                     className="w-12 rounded-full flex-shrink-0 object-cover object-center"
                   />
                   <span className="flex-grow flex flex-col pl-4">
                     <span className="title-font font-medium text-white">
-                      {testimonial.name}
+                      {testimonials.name}
                     </span>
                     <span className="text-gray-500 text-sm uppercase">
-                      {testimonial.company}
+                      {testimonials.company}
                     </span>
                   </span>
                 </div>
