f8bfe99be4a77d7bdc93fa9731c2fc0d8bfe7b24
Jaime Gaspar <jegazhu@gmail.com>
2021-07-25 17:52:09 +0000

    removing comments on Testimonials and adding its variable in data.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
-//import { testimonials } from "../data";
+import { testimonials } from "../data";
 
 export default function Testimonials() {
   return (
