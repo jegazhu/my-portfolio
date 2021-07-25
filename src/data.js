cf99ff5de357f9657903309027e4943a9fb5cbad
Jaime Gaspar <jegazhu@gmail.com>
2021-07-25 17:41:36 +0000

    updating link in data https://codepen.io/trending

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-1.gif",
-    link: "https://github.com/jegazhu/my-portfolio",
+    link: "https://codepen.io/trending",
   },
   {
     title: "React Tracks",
