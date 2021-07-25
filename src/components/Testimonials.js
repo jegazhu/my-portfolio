bb42910e20e9e0fa26da2ff0b584c205440ce048
Jaime Gaspar <jegazhu@gmail.com>
2021-07-25 17:13:12 +0000

    more variables to update

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -11,7 +11,7 @@ export default function Testimonials() {
           Client Testimonials
         </h1>
         <div className="flex flex-wrap m-4">
-          {testimonials.map((testimonial) => (
+          {testimonial.map((testimonial) => (
             <div className="p-4 md:w-1/2 w-full">
               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
                 <TerminalIcon className="block w-8 text-gray-500 mb-4" />
@@ -19,15 +19,15 @@ export default function Testimonials() {
                 <div className="inline-flex items-center">
                   <img
                     alt="testimonial"
-                    src={testimonials.image}
+                    src={testimonial.image}
                     className="w-12 rounded-full flex-shrink-0 object-cover object-center"
                   />
                   <span className="flex-grow flex flex-col pl-4">
                     <span className="title-font font-medium text-white">
-                      {testimonials.name}
+                      {testimonial.name}
                     </span>
                     <span className="text-gray-500 text-sm uppercase">
-                      {testimonials.company}
+                      {testimonial.company}
                     </span>
                   </span>
                 </div>
